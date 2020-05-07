89734a00275209df534b3b296f6c3ace6be48dbb
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-07 20:08:29 +0200

    Finished solution

--- TotalAmountOfPoints/src/Main.java
+++ TotalAmountOfPoints/src/Main.java
@@ -4,6 +4,24 @@ public class Main {
     }
 
     public static int points(String[] games) {
-        return 0;
+
+        int totalPoints = 0;
+        String[] tempArray;
+        int ourTeam = 0;
+        int opponentTeam = 0;
+
+        for (int i = 0; i < games.length; i++){
+            tempArray = games[i].split(":");
+            ourTeam = Integer.valueOf(tempArray[0]);
+            opponentTeam = Integer.valueOf(tempArray[1]);
+            if (ourTeam > opponentTeam){
+                totalPoints += 3;
+            }
+            if (ourTeam == opponentTeam){
+                totalPoints++;
+            }
+        }
+
+        return totalPoints;
     }
 }
