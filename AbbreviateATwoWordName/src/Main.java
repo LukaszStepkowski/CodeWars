8edd0847bc31363bbe3472d43160e8219ae3b7e0
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-07 20:36:41 +0200

    Finished solution

--- AbbreviateATwoWordName/src/Main.java
+++ AbbreviateATwoWordName/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(abbrevName("sAM hARRIS"));
     }
 
     public static String abbrevName(String name) {
+        name = name.replaceAll("\\B[a-zA-Z]", "")
+                .replaceAll(" ", ".")
+                .toUpperCase();
         return name;
     }
 }
