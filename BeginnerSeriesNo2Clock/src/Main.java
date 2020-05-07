3502157e3c1b829efa906b82fb955a34726a1b0a
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-07 19:50:39 +0200

    Finished assignment

--- BeginnerSeriesNo2Clock/src/Main.java
+++ BeginnerSeriesNo2Clock/src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Past(0, 1, 1));
     }
 
     public static int Past(int h, int m, int s)
     {
-
-
-        return 0;
+         return (h * 3600 + m * 60 + s) * 1000;
     }
 }
