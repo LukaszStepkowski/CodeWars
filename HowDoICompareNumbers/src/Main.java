b56791273c66975b75b1ff237fc4755793e184c8
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-08 20:12:24 +0200

    finished solution

--- HowDoICompareNumbers/src/Main.java
+++ HowDoICompareNumbers/src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(whatIs(42 * 42));
     }
 
     public static String whatIs(Integer x) {
         for (Object[] p : specialNumbers) {
-            if (p[0] == x)
+            if (p[0].equals(x)){
                 return (String)p[1];
+            }
         }
         return "nothing";
     }
