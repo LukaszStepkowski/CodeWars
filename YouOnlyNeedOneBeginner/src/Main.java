a4518056c467c4ba067f5ea478f049ff5e8bf797
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-08 19:46:04 +0200

    initial commit

---
+++ YouOnlyNeedOneBeginner/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean check(Object[] a, Object x) {
+        // Your code here
+        return false;
+    }
+}
