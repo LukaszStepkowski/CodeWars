fdea7d12d9bde4c9fc1764866dac0331652727f1
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-13 07:59:43 +0200

    initial commit

---
+++  WillYouMakeIt/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+        return false;
+    }
+}
