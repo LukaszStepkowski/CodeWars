d9dbbfde74d15cef60358709ce5811f710fe89b8
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-13 07:44:41 +0200

    finished solution

--- AllStarCodeChallenge18/src/Main.java
+++ AllStarCodeChallenge18/src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(strCount("Hello", 'z'));
     }
 
     public static int strCount(String str, char letter) {
         int count = 0;
 
+        char[] chars = str.toCharArray();
 
+        for (Character character : chars){
+            if (character.equals(letter)){
+                count++;
+            }
+        }
 
         return count;
     }
