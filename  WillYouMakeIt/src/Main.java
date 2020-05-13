84e322fecb2bbd6cf3404e1da4de83877a3292f1
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-13 08:03:23 +0200

    finished solution

---  WillYouMakeIt/src/Main.java
+++  WillYouMakeIt/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(zeroFuel(50, 25, 2));
     }
 
     public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+        if (distanceToPump / fuelLeft <= mpg) {
+            return true;
+        }
         return false;
     }
 }
