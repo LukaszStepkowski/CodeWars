e258ea8155d26b59fc97c75cc7b3fd60435f337e
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-13 07:39:43 +0200

    initial commit

---
+++ AllStarCodeChallenge18/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int strCount(String str, char letter) {
+        int count = 0;
+
+
+
+        return count;
+    }
+}
