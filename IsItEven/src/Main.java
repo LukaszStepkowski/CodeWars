3a71fa5ab5ad735f4128ec59e565469cfcfd9b1d
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-13 07:56:36 +0200

    finished solution

--- IsItEven/src/Main.java
+++ IsItEven/src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(isEven(-4));
     }
 
-    public boolean isEven(double n) {
-
+    public static boolean isEven(double n) {
+        if (n % 2 == 0){
+            return true;
+        }
         return false;
     }
 }
