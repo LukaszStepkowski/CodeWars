fa83a03f8408042af77a15942818a71dfccc419f
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-14 07:54:45 +0200

    initial commit

---
+++ TripleTrouble/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static String tripleTrouble(String one, String two, String three) {
+        // Solution
+    }
+}
