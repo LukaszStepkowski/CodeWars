95410bf7c8f15261dd702771844eeda152e56c87
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-14 07:29:08 +0200

    initial commit

---
+++ YouCantCodeUnderPressure1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Java {
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i;
+    }
+}
