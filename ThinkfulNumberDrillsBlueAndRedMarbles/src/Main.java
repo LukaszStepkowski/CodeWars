c7e08b2394259c7f63897312bdc379489b569949
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-14 08:25:03 +0200

    finished solution

--- ThinkfulNumberDrillsBlueAndRedMarbles/src/Main.java
+++ ThinkfulNumberDrillsBlueAndRedMarbles/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(guessBlue(5, 5, 2, 3));
     }
 
     public static double guessBlue(int blueStart, int redStart, int bluePulled, int redPulled) {
-        // Place your code here
+        double blueLeft = blueStart - bluePulled;
+        double redLeft = redStart - redPulled;
+
+        return blueLeft / (blueLeft + redLeft);
     }
 }
