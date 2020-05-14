794b80e44a9c90c650b66e85f72e9836c4899895
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-14 08:10:45 +0200

    initial commit

---
+++ ThinkfulNumberDrillsBlueAndRedMarbles/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static double guessBlue(int blueStart, int redStart, int bluePulled, int redPulled) {
+        // Place your code here
+    }
+}
