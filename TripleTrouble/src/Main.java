cc188055fa406ccf18bba9ffb458f1fa5c5c740c
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-14 08:01:07 +0200

    finished solution

--- TripleTrouble/src/Main.java
+++ TripleTrouble/src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(tripleTrouble("Bm", "aa", "tn"));
     }
 
     public static String tripleTrouble(String one, String two, String three) {
-        // Solution
+        String output = "";
+        for (int i = 0; i < one.length(); i++){
+            output = output + one.charAt(i) + two.charAt(i) + three.charAt(i);
+        }
+        return output;
     }
 }
