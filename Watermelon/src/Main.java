c06d3ef86845955140f2feed1df519f0065ae099
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-14 07:51:22 +0200

    initial commit

---
+++ Watermelon/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean divide(int weight) {
+        // your code here
+        return false;
+    }
+}
