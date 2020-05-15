d59951ee644e67ab2637e2902671d322330237e9
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-15 08:18:59 +0200

    finished solution

---
+++ ExclusiveOrXorLogicalOperator/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean xor(boolean a, boolean b) {
+        if (a != b) {
+            return true;
+        } else return false;
+    }
+}
