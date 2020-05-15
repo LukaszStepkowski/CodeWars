3cbb0a8d43b0c49ffbfc46bfde24444b30815258
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-15 08:09:56 +0200

    finished solution

--- NoZerosForHeros/src/Main.java
+++ NoZerosForHeros/src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(noBoringZeros(960000));
     }
 
     public static int noBoringZeros(int n) {
-
+        for (int i = 0; i  < 10; i++){
+            if (n % 10 == 0) {
+                n = n / 10;
+            }
+        }
+        return n;
     }
 }
