a2dbadfae73d2c7560c4acbdc7fd7e0a6cac7b0d
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-15 07:02:43 +0200

    finished solution

--- GrasshopperGradeBook/src/Main.java
+++ GrasshopperGradeBook/src/Main.java
@@ -1,10 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(getGrade(84, 79, 85));
     }
 
     public static char getGrade(int s1, int s2, int s3) {
+        char grade = ' ';
+        int avgScore = (s1 + s2 + s3) / 3;
 
-        return '';
+        if (avgScore < 60){
+            grade = 'F';
+        } else if (avgScore < 70) {
+            grade = 'D';
+        } else if (avgScore < 80) {
+            grade = 'C';
+        } else if (avgScore < 90) {
+            grade = 'B';
+        } else grade = 'A';
+
+        return grade;
     }
 }
