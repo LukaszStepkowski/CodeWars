58e29436c32aa91732f03456d355a2081a91cd4f
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-15 07:26:26 +0200

    finished solution

--- GrasshopperSummation/src/Main.java
+++ GrasshopperSummation/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(summation(8));
     }
 
     public static int summation(int n) {
-
-        return 0;
+        if (n == 0){
+            return 0;
+        } else return n + summation(n - 1);
     }
 }
