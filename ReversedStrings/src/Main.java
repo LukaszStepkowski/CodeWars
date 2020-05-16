3956e5997613764dac0e03f684d60dca2bd79c21
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-16 07:57:50 +0200

    finished solution

--- ReversedStrings/src/Main.java
+++ ReversedStrings/src/Main.java
@@ -1,10 +1,12 @@
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(solution("olleH"));
     }
 
     public static String solution(String str) {
-        // Your code here...
-        return "";
+        StringBuilder sb = new StringBuilder();
+        sb.append(str).reverse();
+        return sb.toString();
     }
 }
