84d8cec3f6e38b96abb11878a0557d676a1a987e
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-16 08:08:51 +0200

    finished solution

--- ReversedWords/src/Main.java
+++ ReversedWords/src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(reverseWords("The greatest victory is that which requires no battle"));
     }
 
     public static String reverseWords(String str){
-        //write your code here...
+        String[] words = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = words.length; i > 0; i--){
+            sb.append(words[i-1]).append(" ");
+        }
+        return sb.toString().trim();
     }
 }
