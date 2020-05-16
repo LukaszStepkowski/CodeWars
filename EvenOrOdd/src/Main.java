1a288fec162b1196c418a5c71b0aa1937352e148
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-16 16:47:50 +0200

    finished solution

--- EvenOrOdd/src/Main.java
+++ EvenOrOdd/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(even_or_odd(-58));
     }
 
     public static String even_or_odd(int number) {
-
+        return (number % 2 == 0) ? "Even" : "Odd";
     }
 }
