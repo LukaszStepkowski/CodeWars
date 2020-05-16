f0b1fed8d2318431d5d9c3851c456a6ed5507e17
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-16 17:45:13 +0200

    finished solution

--- 8Kyu/src/ZywOo.java
+++ 8Kyu/src/ZywOo.java
@@ -1,5 +1,14 @@
 public class ZywOo {
     public static int[] take(int[] arr, int n) {
-        return //your code;
+        int[] output;
+        if (n > arr.length){
+            output = new int[arr.length];
+        } else {
+            output = new int[n];
+        }
+        for (int i = 0; i < output.length; i++) {
+            output[i] = arr[i];
+        }
+        return output;
     }
 }
