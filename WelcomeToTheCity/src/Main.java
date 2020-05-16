4a5101c556956c6d5e7b85da6b2659bb53020c58
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-16 15:39:01 +0200

    finished solution

--- WelcomeToTheCity/src/Main.java
+++ WelcomeToTheCity/src/Main.java
@@ -5,6 +5,21 @@ public class Main {
     }
 
     public static String sayHello(String [] name, String city, String state){
+        StringBuilder sb = new StringBuilder();
+        StringBuilder output = new StringBuilder();
+        for (String string : name) {
+            sb.append(string).append(" ");
+        }
+        String fullName = sb.toString().trim();
 
+        output.append("Hello, ")
+                .append(fullName)
+                .append("! Welcome to ")
+                .append(city)
+                .append(", ")
+                .append(state)
+                .append("!");
+
+        return output.toString();
     }
 }
