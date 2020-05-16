762225932701b4ccef324fcbe5bce5701d1ff254
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-16 16:51:16 +0200

    initial commit

---
+++ SentenceSmash/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static String smash(String... words) {
+        // TODO Write your code below this comment please
+    }
+}
