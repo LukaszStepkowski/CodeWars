01626935f8a30e04d77d4ba3922c9a01bd2da741
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-16 08:20:18 +0200

    finished solution

--- KataExampleTwist/src/Main.java
+++ KataExampleTwist/src/Main.java
@@ -3,9 +3,11 @@ public class Main {
 
     }
 
-    public static String[] kataExampleTwist()
-    {
-        String[] websites = {};
+    public static String[] kataExampleTwist() {
+        String[] websites = new String[1000];
+        for (int i = 0; i < 1000; i++) {
+            websites[i] = "codewars";
+        }
         return websites;
     }
 }
