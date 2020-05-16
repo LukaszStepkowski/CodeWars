bc23afcd0917b9b75356842495c88558ce2e1d16
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-16 15:38:25 +0200

    initial commit

---
+++ WelcomeToTheCity/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] name = {"John", "Smith"};
+        System.out.println(sayHello(name, "Phoenix", "Arizona"));
+    }
+
+    public static String sayHello(String [] name, String city, String state){
+
+    }
+}
