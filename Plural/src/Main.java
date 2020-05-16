348ebf375bdc9f399b8fa756fd09971f7ce268c1
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-16 15:25:41 +0200

    finished solution

---
+++ Plural/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isPlural(float f){
+        if (f == 1){
+            return false;
+        } else return true;
+    }
+}
