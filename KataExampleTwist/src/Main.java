b3e056b7d378ffb4453dce20c22b78e45cd95a3d
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-16 08:18:12 +0200

    initial commit

---
+++ KataExampleTwist/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static String[] kataExampleTwist()
+    {
+        String[] websites = {};
+        return websites;
+    }
+}
