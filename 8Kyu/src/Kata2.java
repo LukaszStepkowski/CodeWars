c98f49acf3aed074d59482570eb48a5ddc2f5a10
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-17 15:54:29 +0200

    finished solution

--- 8Kyu/src/Kata2.java
+++ 8Kyu/src/Kata2.java
@@ -1,5 +1,12 @@
 public class Kata2 {
     public static int[] digitize(long n) {
-        // Code here
+        String temp = String.valueOf(n);
+        String[] strArr = temp.split("");
+        int[] opt = new int[strArr.length];
+
+        for (int i = 0; i < strArr.length; i++){
+            opt[i] = Integer.parseInt(strArr[strArr.length - i - 1]);
+        }
+        return opt;
     }
 }
