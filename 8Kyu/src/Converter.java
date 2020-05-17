6d49c98ad08c847b2ca0f09f5a2f6e766a93976d
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-17 15:14:04 +0200

    finished solution

--- 8Kyu/src/Converter.java
+++ 8Kyu/src/Converter.java
@@ -1,5 +1,8 @@
 public class Converter {
     public static float mpgToKPM(final float mpg) {
-
+        float mileToKilometer = 1.60934f;
+        float gallonToLiter = 4.5460918f;
+        float output = Math.round((mpg * mileToKilometer / gallonToLiter) * 100);
+        return output / 100;
     }
 }
