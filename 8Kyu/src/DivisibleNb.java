f785d236eddacf51e395a438a709f7bf10a2e187
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-17 07:36:36 +0200

    finished solution

--- 8Kyu/src/DivisibleNb.java
+++ 8Kyu/src/DivisibleNb.java
@@ -1,5 +1,5 @@
 public class DivisibleNb {
     public static boolean isDivisible(long n, long x, long y) {
-        // your code
+        return (n % x == 0 && n % y == 0) ? true : false;
     }
 }
