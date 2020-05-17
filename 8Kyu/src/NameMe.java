8b1765e0ba0ab88fb45cd49a70ce87cddfa80ecd
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-17 07:13:05 +0200

    started "this" is a problem kata

---
+++ 8Kyu/src/NameMe.java
@@ -0,0 +1,10 @@
+public class NameMe {
+    private String firstName;
+    private String lastName;
+    private String fullName;
+
+    public NameMe(String first, String last) {
+        this.firstName = first;
+        this.lastName = last;
+    }
+}
