2970a676a1f1a1ec62dc318615542e802cd9c7a2
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-17 07:14:52 +0200

    finished solution

--- 8Kyu/src/NameMe.java
+++ 8Kyu/src/NameMe.java
@@ -6,5 +6,18 @@ public class NameMe {
     public NameMe(String first, String last) {
         this.firstName = first;
         this.lastName = last;
+        this.fullName = first + " " + last;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
     }
 }
