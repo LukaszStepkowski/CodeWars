ea662ab16b886bde213ea478b234eeace3107393
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-18 07:48:17 +0200

    finished solution

--- 8Kyu/src/SmallestIntegerFinder.java
+++ 8Kyu/src/SmallestIntegerFinder.java
@@ -4,6 +4,12 @@ public class SmallestIntegerFinder {
     }
 
     public static int findSmallestInt(int[] args) {
-
+        int opt = args[0];
+        for (int i = 0; i < args.length; i++){
+            if (opt > args[i]){
+                opt = args[i];
+            }
+        }
+        return opt;
     }
 }
