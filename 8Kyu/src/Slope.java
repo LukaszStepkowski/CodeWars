74ae967c5c2ae9645020b04477e253ce61aa1eb2
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-18 07:56:42 +0200

    finished solution

--- 8Kyu/src/Slope.java
+++ 8Kyu/src/Slope.java
@@ -1,6 +1,8 @@
 public class Slope {
     public String slope(int[] points)
     {
-
+        if (points[2] - points[0] == 0){
+            return "undefined";
+        } else return String.valueOf((points[3] - points[1]) / (points[2] - points[0]));
     }
 }
