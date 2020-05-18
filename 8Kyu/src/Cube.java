18c14108885d13961fe0b7da5dab0369be6fd3c7
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-18 08:07:30 +0200

    finished solution

--- 8Kyu/src/Cube.java
+++ 8Kyu/src/Cube.java
@@ -1,3 +1,14 @@
 public class Cube {
-    
+    private int side;
+
+    public Cube() {
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
 }
