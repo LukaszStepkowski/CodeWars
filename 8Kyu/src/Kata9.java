ef8d82a1d6aabdacebfcdf2f11f64c7edd87e51b
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-21 08:05:51 +0200

    finished solution

--- 8Kyu/src/Kata9.java
+++ 8Kyu/src/Kata9.java
@@ -1,5 +1,17 @@
 public class Kata9 {
     public static String rps(String p1, String p2) {
-        // your code
+        if (p1.equals("rock") && p2.equals("paper")){
+            return "Player 2 won!";
+        } else if (p1.equals("paper") && p2.equals("scissors")){
+            return "Player 2 won!";
+        } else if (p1.equals("scissors") && p2.equals("rock")){
+            return "Player 2 won!";
+        } else if (p1.equals("rock") && p2.equals("scissors")){
+            return "Player 1 won!";
+        } else if (p1.equals("paper") && p2.equals("rock")){
+            return "Player 1 won!";
+        } else if (p1.equals("scissors") && p2.equals("paper")){
+            return "Player 1 won!";
+        } else return "Draw!";
     }
 }
