5228ab9b228532b539ff47f9d857e2d67f92adf8
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-21 07:29:16 +0200

    finished solution

--- 8Kyu/src/Kata7.java
+++ 8Kyu/src/Kata7.java
@@ -1,7 +1,8 @@
 public class Kata7 {
     public static int[] getSize(int w,int h,int d) {
         int[] opt = new int[2];
-
+        opt[0] = 2 * (w * h) + 2 * (w * d) + 2 * (h * d);
+        opt[1] = w * h * d;
         return opt;
     }
 }
