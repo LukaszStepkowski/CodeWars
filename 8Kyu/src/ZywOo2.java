1149edf7e5c9961d5b299b8476b9a6818b104daf
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-22 08:04:05 +0200

    finished solution

--- 8Kyu/src/ZywOo2.java
+++ 8Kyu/src/ZywOo2.java
@@ -1,6 +1,5 @@
 public class ZywOo2 {
     public static boolean validateUsr(String s) {
-        //glhf:)
-        return s;
+        return (s.matches("[a-z0-9_]{4,16}")) ? true : false;
     }
 }
