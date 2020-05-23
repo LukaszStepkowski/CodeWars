c09fe6d2eda4cd2eddd060f13779d69e3deb48a4
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-23 15:01:30 +0200

    finished Printing Array elements with Comma delimiters kata

---
+++ 8Kyu/src/ArrayPrinter.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class ArrayPrinter {
+    public static String printArray(Object array[]) {
+        StringBuilder sb = new StringBuilder();
+        for (Object o : array){
+            sb.append(o).append(",");
+        }
+        return sb.toString().substring(0, sb.length()-1);
+    }
+}
