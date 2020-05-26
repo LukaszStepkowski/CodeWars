1e256cdb32fe30a016ab7ac17e86e7360c3fd016
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-26 07:33:54 +0200

    finished solution

--- 8Kyu/src/WrongEndHead.java
+++ 8Kyu/src/WrongEndHead.java
@@ -1,5 +1,9 @@
 public class WrongEndHead {
     public static String[] fixTheMeerkat(String[] arr) {
-        return null;
+        String[] opt = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            opt[i] = arr[arr.length - 1 - i];
+        }
+        return opt;
     }
 }
