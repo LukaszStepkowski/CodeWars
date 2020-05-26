fc46806fcf78f3297fbcadffb2feac386753113f
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-26 08:19:49 +0200

    finished How many stairs will Suzuki climb in 20 years? kata

---
+++ 8Kyu/src/Kata17.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Kata17 {
+    public static long stairsIn20(int[][] stairs) {
+        return 20 * Arrays.stream(stairs).
+                mapToLong(i -> IntStream.of(i).sum())
+                .sum();
+    }
+}
