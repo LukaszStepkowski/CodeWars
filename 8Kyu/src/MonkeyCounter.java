165b61c6a4bab1ee40f5f6ae1faa5f760292ab08
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-26 07:41:16 +0200

    finished solution

--- 8Kyu/src/MonkeyCounter.java
+++ 8Kyu/src/MonkeyCounter.java
@@ -1,5 +1,7 @@
+import java.util.stream.IntStream;
+
 public class MonkeyCounter {
     public static int[] monkeyCount(final int n){
-
+        return IntStream.rangeClosed(1, n).toArray();
     }
 }
