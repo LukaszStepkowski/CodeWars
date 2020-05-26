adc9ce157962e2cdc05f7ab959b469be7d54082c
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-26 08:25:34 +0200

    finished solution

--- 8Kyu/src/GhostCode.java
+++ 8Kyu/src/GhostCode.java
@@ -1,9 +1,6 @@
 public class GhostCode {
     public static String helloName(final String name){
-        // \u000d name="dumb";
-        if(name == null || name.equals(""))
-            return "Hello world!";
-        else
-            return "Hello my name is " + name;
+
+        return (name !=null && !name.isEmpty()) ? "Hello my name is " + name : "Hello world!";
     }
 }
