fb2e920103903bf56c6097920bb34241269f1736
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-28 08:40:06 +0200

    finished Area of a Square kata

---
+++ 8Kyu/src/Geometry.java
@@ -0,0 +1,9 @@
+import java.text.DecimalFormat;
+
+public class Geometry {
+    public static double squareArea(double A){
+        DecimalFormat df = new DecimalFormat("#.##");
+        double r = (2 * A) / Math.PI;
+        return Double.valueOf(df.format(Math.pow(r, 2)));
+    }
+}
