e92db6cd2c647ffcb219798f559cb7fcf0b76943
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-05-31 11:44:37 +0200

    finished Well of Ideas - Easy Version kata

---
+++ 8Kyu/src/Kata29.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Kata29 {
+    public static String well(String[] x) {
+        long goodIdeas = Arrays.stream(x).filter(i -> i.equals("good")).count();
+        return (goodIdeas == 0) ? "Fail!" : (goodIdeas < 3) ? "Publish!" : "I smell a series!";
+    }
+}
