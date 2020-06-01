960fe020d048222c1e786f9a3cbce10d9ed11733
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-01 07:45:08 +0200

    finished Beginner - Lost Without a Map - kata

---
+++ 8Kyu/src/Maps.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Maps {
+    public static int[] map(int[] arr) {
+        return Arrays.stream(arr)
+                .map(i -> i *=2)
+                .toArray();
+    }
+}
