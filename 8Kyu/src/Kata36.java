6f865f04a232bbdaf8e1e4dabeae1ff96cf140d6
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-02 08:11:01 +0200

    finished Volume of a Cuboid kata

---
+++ 8Kyu/src/Kata36.java
@@ -0,0 +1,6 @@
+public class Kata36 {
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        // Your code here
+        return length * width * height;
+    }
+}
