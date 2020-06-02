0db0d2a1e30a47928e1938372d1db83ebc02ac8b
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-02 08:03:09 +0200

    finished Switch it Up! kata

---
+++ 8Kyu/src/Kata34.java
@@ -0,0 +1,26 @@
+public class Kata34 {
+    public static String switchItUp(int number)
+    {
+        switch (number){
+            case 0:
+                return "Zero";
+            case 1:
+                return "One";
+            case 2:
+                return "Two";
+            case 3:
+                return "Three";
+            case 4:
+                return "Four";
+            case 5:
+                return "Five";
+            case 6:
+                return "Six";
+            case 7:
+                return "Seven";
+            case 8:
+                return "Eight";
+            default: return "Nine";
+        }
+    }
+}
