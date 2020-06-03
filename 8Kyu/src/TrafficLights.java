c4c86cc23ddbe0c248cc05eb3842375aed9a01f6
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-03 07:34:36 +0200

    finished Thinkful - Logic Drills: Traffic light kata

---
+++ 8Kyu/src/TrafficLights.java
@@ -0,0 +1,5 @@
+public class TrafficLights {
+    public static String updateLight(String current) {
+        return (current.equals("red")) ? "green" : (current.equals("green") ? "yellow" : "red");
+    }
+}
