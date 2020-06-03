9c87015fd76f769294502b7e4f704d43957bd9a8
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-03 08:14:36 +0200

    finished Find the first non-consecutive number kata

---
+++ 8Kyu/src/FirstNonConsecutive.java
@@ -0,0 +1,14 @@
+public class FirstNonConsecutive {
+    static Integer find(final int[] array) {
+        Integer prev = array[0];
+        Integer first = null;
+        for (Integer i = 1; i < array.length; i++){
+            if ((prev+1) != array[i]){
+                    first = array[i];
+                    break;
+            }
+            prev++;
+        }
+        return first;
+    }
+}
