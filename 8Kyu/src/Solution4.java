b436428b4be131fca5993d6a6dee06b1f17e3c10
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-08 08:28:54 +0200

    finished Find the force of gravity between two objects kata

---
+++ 8Kyu/src/Solution4.java
@@ -0,0 +1,13 @@
+public class Solution4 {
+    public static double solution(double[] arrVal, String[] arrUnit) {
+
+        double newtons = 6.67e-11 * arrVal[0] * arrVal[1] / (arrVal[2] * arrVal[2]);
+
+        for (String el : arrUnit) {  // convert like a boss
+            newtons /= (el=="g") ? 1e3 : (el=="mg") ? 1e6 : (el=="μg") ? 1e9 : (el=="ft") ? 0.3048 * 0.3048 : 1;
+            newtons *= (el=="lb") ? 0.453592 : (el=="cm") ? 1e4 : (el=="mm") ? 1e6 : (el=="μm") ? 1e12 : 1;
+        }
+
+        return newtons;
+    }
+}
