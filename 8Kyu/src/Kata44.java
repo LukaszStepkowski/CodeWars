c0ea88745414bfa46a5d65a2ad8ce016e6993737
Lukasz Stepkowski <l.stepkow@gmail.com>
2020-06-08 07:44:36 +0200

    finished Expressions Matter kata

---
+++ 8Kyu/src/Kata44.java
@@ -0,0 +1,6 @@
+public class Kata44 {
+    public static int expressionsMatter(int a, int b, int c)
+    {
+        return Math.max(Math.max(a + b + c, a * b * c), Math.max((a + b) * c, a * (b + c)));
+    }
+}
